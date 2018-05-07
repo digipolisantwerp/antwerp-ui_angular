fdf19b85a99479aa5f636cb47e1738882a79eb23
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-05-07 17:21:58 +0200

    add coverage serve

--- config/base.config.js
+++ config/base.config.js
@@ -1,13 +1,15 @@
 module.exports = {
     "coveragePathIgnorePatterns": [
         "/node_modules/",
-        "index.ts"
+        "index.ts",
+        "config",
+        ".+\.html",
     ],
     "modulePathIgnorePatterns": [
         "dist/*",
-        "npm-cache",
-        ".npm",
-        "config",
+        "npm-cache/*",
+        ".npm/*",
+        "config/*",
     ],
     "testMatch": [
         "**/__tests__/**/*.+(ts|js)?(x)",
