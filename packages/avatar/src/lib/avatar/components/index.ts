18a47b21d1a8e75ae858f7941a3dbc8233159497
Fabian Meul <fabian.meul@gmail.com>
2018-05-29 17:22:35 +0200

    Added avatar-module

---
+++ packages/avatar/src/lib/avatar/components/index.ts
@@ -0,0 +1,11 @@
+import { AvatarComponent } from './avatar/avatar.component';
+
+const Components = [
+	AvatarComponent,
+];
+
+export {
+	Components,
+
+	AvatarComponent,
+};
---
+++ packages/avatar/src/lib/avatar/index.ts
@@ -0,0 +1,2 @@
+export { AvatarModule } from './avatar.module';
+export { AvatarComponent } from './components/avatar/avatar.component';
