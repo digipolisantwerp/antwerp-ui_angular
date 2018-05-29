a32fd52173dd58e791814337e521bbd23fa64425
Fabian Meul <fabian.meul@gmail.com>
2018-05-29 14:31:31 +0200

    Add code-snippet module

---
+++ packages/code-snippet/src/lib/code-snippet/code-snippet.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HighlightJsModule, HighlightJsService } from 'angular2-highlight-js';
+
+import { CodeSnippetComponent } from './components/code-snippet.component';
+
+@NgModule({
+	imports: [
+		CommonModule,
+		HighlightJsModule,
+	],
+	declarations: [
+		CodeSnippetComponent,
+	],
+	providers: [
+		HighlightJsService,
+	],
+	exports: [
+		CodeSnippetComponent,
+	],
+})
+export class CodeSnippetModule {
+}
