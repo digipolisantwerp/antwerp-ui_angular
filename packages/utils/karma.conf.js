4b7a145b02374aede5f270a5f61b32bea796baf0
Fabian Meul <fabian.meul@gmail.com>
2018-06-04 17:47:31 +0200

    Run tests with ChromeHeadless

--- packages/utils/karma.conf.js
+++ packages/utils/karma.conf.js
@@ -25,15 +25,7 @@ module.exports = function (config) {
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+		browsers: ['ChromeHeadless'],
 		singleRun: false,
-
-		// you can define custom flags
-		customLaunchers: {
-			ChromeHeadlessNoSandbox: {
-				base: 'ChromeHeadless',
-				flags: ['--no-sandbox']
-			}
-		}
 	});
 };
