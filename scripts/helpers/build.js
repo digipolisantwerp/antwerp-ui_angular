2ae7aec9f00116e63c94705a9faf8a10706a97fd
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-06-12 12:07:20 +0200

    fix(setup): cleaner build logs

--- scripts/helpers/build.js
+++ scripts/helpers/build.js
@@ -1,7 +1,8 @@
+const colors = require('colors');
 const exec = require('./bash');
 
-module.exports = package => {
-  console.log(`Building ${package}...`);
+module.exports = package => () => {
+	console.log(colors.yellow(`Building ${package}...`));
 
-  return () => exec(`ng build ${package}`);
+	return exec(`ng build ${package}`);
 };
