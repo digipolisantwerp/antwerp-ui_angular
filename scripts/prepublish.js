4556fd0de1017c23bcff5f8dc067bc870eedd5dc
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-06-12 11:51:59 +0200

    fix(setup): correct result in prepublish

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,19 +1,18 @@
 const cpx = require('cpx');
-const { readFileSync, writeFileSync } = require('fs');
+const { writeFileSync } = require('fs');
 const { getNPMDependencies } = require('./helpers/deps');
 
 cpx.copy('LICENSE', 'dist');
 cpx.copy('README.md', 'dist');
 cpx.copy('CHANGELOG.md', 'dist');
 
-const packageJson = JSON.parse(readFileSync('package.json'));
-const dependencies = getNPMDependencies({ preserve: true });
+const packageJson = getNPMDependencies({ preserve: true });
 
 delete packageJson['scripts'];
 delete packageJson['private'];
 delete packageJson['devDependencies'];
 delete packageJson['publishConfig'];
 
-writeFileSync('dist/package.json', JSON.stringify(result, undefined, 2));
+writeFileSync('dist/package.json', JSON.stringify(packageJson, undefined, 2));
 
 process.exit();
