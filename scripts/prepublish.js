ad3991030e51309a3ba695281e6587c79e7e3b7d
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-06-12 11:44:26 +0200

    refactor(setup): cleanup prepublish script with new helper

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,26 +1,19 @@
-const mergePackages = require('@userfrosting/merge-package-dependencies');
 const cpx = require('cpx');
-const fs = require('fs');
-const { lstatSync, readdirSync } = require('fs');
-const { join, resolve } = require('path');
+const { readFileSync, writeFileSync } = require('fs');
+const { getNPMDependencies } = require('./helpers/deps');
 
 cpx.copy('LICENSE', 'dist');
 cpx.copy('README.md', 'dist');
 cpx.copy('CHANGELOG.md', 'dist');
 
-const packageJson = JSON.parse(fs.readFileSync('package.json'));
+const packageJson = JSON.parse(readFileSync('package.json'));
+const dependencies = getNPMDependencies({ preserve: true });
+
 delete packageJson['scripts'];
 delete packageJson['private'];
+delete packageJson['devDependencies'];
+delete packageJson['publishConfig'];
 
-const isDirectory = source => lstatSync(source).isDirectory();
-const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory);
-
-const packages = getDirectories(resolve(process.cwd(), 'packages')).map(path => `${path}/`);
-
-const result = mergePackages.npm(packageJson, packages);
-
-delete result['devDependencies'];
-
-fs.writeFileSync('dist/package.json', JSON.stringify(result, undefined, 2));
+writeFileSync('dist/package.json', JSON.stringify(result, undefined, 2));
 
 process.exit();
