4c5f8eb531c3fd3d93e03ba012c8b201812c2dc8
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-06-12 11:45:03 +0200

    feat(setup): update npm depencendies in bootstrap
    * fetch merged dependencies & devdependencies
    * add as devdependencies to root

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,14 +1,36 @@
 const { resolve } = require('path');
 const colors = require('colors');
+const { readFileSync, writeFileSync } = require('fs');
+const sortKeys = require('sort-object-keys');
+
 const exec = require('./helpers/bash');
 const { getDirectories } = require('./helpers/dir');
 const promiseQueue = require('./helpers/queue');
+const { getNPMDependencies } = require('./helpers/deps');
 
 const directories = getDirectories(resolve(process.cwd(), 'packages'));
 
-promiseQueue(directories.map(directory => {
-    return () => exec(`cd ${directory} && npm install`);
-}))
+const updateNpmDependencies = () => {
+	const packageJson = JSON.parse(readFileSync('package.json'));
+	const npmDependencies = getNPMDependencies();
+
+	writeFileSync('package.json', JSON.stringify(Object.assign({}, packageJson, {
+		devDependencies: sortKeys(Object.assign({},
+			packageJson.devDependencies,
+			npmDependencies.devDependencies,
+			npmDependencies.dependencies,
+		)),
+	}), undefined, 2));
+
+	return exec('npm install');
+};
+
+promiseQueue([
+	updateNpmDependencies(),
+	...directories.map(directory => {
+		return () => exec(`cd ${directory} && npm install`);
+	})
+])
     .then(() => {
         console.log(colors.green('Bootstrap completed.'));
         process.exit();
