6e8e42f70b17344af3fa63fc240e9a1c14347794
Fabian Meul <fabian.meul@gmail.com>
2018-06-18 14:58:42 +0200

    fix(ci): handle spaces in folder names

--- packages/forms/src/lib/field-errors/components/field-error/field-error.component.ts
+++ packages/forms/src/lib/field-errors/components/field-error/field-error.component.ts
@@ -5,7 +5,9 @@ import { FieldError } from '../../types/field-errors.types';
 @Component({
 	selector: 'aui-field-error',
 	templateUrl: './field-error.component.html',
-	styleUrls: ['./field-error.component.scss'],
+	styleUrls: [
+		'./field-error.component.scss',
+	],
 })
 export class FieldErrorComponent {
 	@Input() public error: FieldError;
