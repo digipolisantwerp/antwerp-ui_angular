b50bdf17de0f11f693c6c9c72d1cda74e78595c2
Jasper De Smet <jasp.desmet@gmail.com>
2018-06-18 15:43:25 +0200

    fix(styleguide) fix unit tests

--- styleguide/app/app.component.spec.ts
+++ styleguide/app/app.component.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+		schemas: [CUSTOM_ELEMENTS_SCHEMA],
+		imports: [
+			RouterTestingModule,
+		],
 		declarations: [
 			AppComponent,
 		],
@@ -13,16 +19,5 @@ describe('AppComponent', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app).toBeTruthy();
-	}));
-	it(`should have as title 'app'`, async(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual('app');
-	}));
-	it('should render title in a h1 tag', async(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
 	}));
 });
