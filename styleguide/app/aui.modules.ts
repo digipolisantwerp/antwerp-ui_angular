369b26d38d4fac0c110880e994894902f367d539
Jasper De Smet <jasp.desmet@gmail.com>
2018-06-18 15:22:59 +0200

    fix(styleguide) fix linting errors

--- styleguide/app/aui.modules.ts
+++ styleguide/app/aui.modules.ts
@@ -1,10 +1,10 @@
-import { HeaderModule, HeroModule, FooterModule, PaneModule } from "@acpaas-ui/ngx-components/layout";
-import { LogoModule } from "@acpaas-ui/ngx-components/logo";
+import { HeaderModule, HeroModule, FooterModule, PaneModule } from '@acpaas-ui/ngx-components/layout';
+import { LogoModule } from '@acpaas-ui/ngx-components/logo';
 
 export const AUIModules = [
-    HeaderModule,
-    HeroModule,
-    FooterModule,
-    PaneModule,
-    LogoModule,
+	HeaderModule,
+	HeroModule,
+	FooterModule,
+	PaneModule,
+	LogoModule,
 ];
