ae00c89d157c6165406ec49212ae63bfd37a2600
Jasper De Smet <jasp.desmet@gmail.com>
2018-06-19 11:44:20 +0200

    fix(forms) fix masks build by using require for InputMask

--- packages/forms/src/lib/mask/directives/mask.directive.ts
+++ packages/forms/src/lib/mask/directives/mask.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
-import * as Inputmask from 'inputmask';
+const Inputmask = require('inputmask');
 
 @Directive({
 	selector: '[auiMask]',
