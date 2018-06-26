ae7cc4c5dd9b1407a74e57a3a56bf993deb74574
Jasper De Smet <jasp.desmet@gmail.com>
2018-06-26 11:52:27 +0200

    polish(styleguide) remove all unused links

--- styleguide/app/pages/modules/modules.page.ts
+++ styleguide/app/pages/modules/modules.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Routes, Router, ActivatedRoute } from '@angular/router';
 
 import { EXAMPLES_ROUTES } from '../../examples.routes';
 
@@ -7,6 +7,17 @@ import { EXAMPLES_ROUTES } from '../../examples.routes';
 	templateUrl: './modules.page.html',
 	styleUrls: ['./modules.page.scss'],
 })
-export class ModulesPageComponent {
+export class ModulesPageComponent implements OnInit {
 	public packages: Routes = EXAMPLES_ROUTES;
+
+	constructor(
+		private router: Router,
+		private activatedRoute: ActivatedRoute,
+	) {}
+
+	public ngOnInit() {
+		if (this.activatedRoute.snapshot.children.length === 0) {
+			this.router.navigate([ this.packages[0].path ], { relativeTo: this.activatedRoute });
+		}
+	}
 }
