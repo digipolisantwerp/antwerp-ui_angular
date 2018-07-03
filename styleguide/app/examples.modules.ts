1ab4c334a9c9b88e28274400835f2b5f2b5f4898
Jasper De Smet <jasp.desmet@gmail.com>
2018-07-03 08:58:55 +0200

    feat(calendar) add example for calendar

--- styleguide/app/examples.modules.ts
+++ styleguide/app/examples.modules.ts
@@ -1,7 +1,9 @@
 import { AvatarExamplesModule } from '@acpaas-ui/ngx-examples/avatar';
+import { CalendarExamplesModule } from '@acpaas-ui/ngx-examples/calendar';
 import { LogoExamplesModule } from '@acpaas-ui/ngx-examples/logo';
 
 export const ExamplesModules = [
 	AvatarExamplesModule,
+	CalendarExamplesModule,
 	LogoExamplesModule,
 ];
--- styleguide/app/examples.routes.ts
+++ styleguide/app/examples.routes.ts
@@ -1,7 +1,9 @@
 import { AVATAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/avatar';
+import { CALENDAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/calendar';
 import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo';
 
 export const EXAMPLES_ROUTES = [
 	{ path: 'avatar', children: AVATAR_EXAMPLES_ROUTES },
+	{ path: 'calendar', children: CALENDAR_EXAMPLES_ROUTES },
 	{ path: 'logo', children: LOGO_EXAMPLES_ROUTES },
 ];
