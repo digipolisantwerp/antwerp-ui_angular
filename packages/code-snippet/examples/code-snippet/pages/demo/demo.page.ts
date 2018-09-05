acd39221758a5cf1bfcb00e3a6ecbc8ed056ad46
ICA\rc01673 <tom.vandenbroeck@digipolis.be>
2018-09-05 16:05:59 +0200

    example

--- packages/code-snippet/examples/code-snippet/pages/demo/demo.page.ts
+++ packages/code-snippet/examples/code-snippet/pages/demo/demo.page.ts
@@ -4,8 +4,28 @@ import { Component } from '@angular/core';
 	templateUrl: './demo.page.html',
 })
 export class CodeSnippetDemoPageComponent {
-	// public importModule = 'import { CodeSnippetModule } from \'@acpaas-ui/ngx-components/code-snippet\';';
-	// public example1 = '<aui-avatar image="https://robohash.org/acpaas-ui" title="My image"></aui-avatar>';
-	// public example2 = '<aui-avatar icon="fa fa-user" title="An icon" size="L"></aui-avatar>';
-	// public example3 = '<aui-avatar letter="T" title="The letter T" size="S"></aui-avatar>';
+	public codeExampleJSON = `
+	[
+	    {
+	        "title": "apples",
+	        "count": [12000, 20000],
+	        "description": {"text": "...", "sensitive": false}
+	    },
+	    {
+	        "title": "oranges",
+	        "count": [17500, null],
+	        "description": {"text": "...", "sensitive": false}
+	    }
+	]`;
+
+	public codeExampleJS = `
+	function greetMe(yourName) {
+        alert('Hello ' + yourName);
+    }
+	greetMe('World');`;
+
+	public importModule = 'import { CodeSnippetModule } from \'@acpaas-ui/ngx-components/code-snippet\';';
+	public example1 = '<aui-code-snippet [codeSnippet]="Single text example"></aui-code-snippet>';
+	public example2 = '<aui-code-snippet [codeSnippet]="codeExampleJSON"></aui-code-snippet>';
+	public example3 = '<aui-code-snippet [codeSnippet]="codeExampleJS"></aui-code-snippet>';
 }
