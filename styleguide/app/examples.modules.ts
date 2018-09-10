317eb5ac8830bfd25e476d0453d8cab0d2816f5a
ICA\rc01673 <tom.vandenbroeck@digipolis.be>
2018-09-10 14:12:38 +0200

    run styleguide

--- styleguide/app/examples.modules.ts
+++ styleguide/app/examples.modules.ts
@@ -1,15 +1,11 @@
 import { AvatarExamplesModule } from '@acpaas-ui/ngx-examples/avatar';
 import { CalendarExamplesModule } from '@acpaas-ui/ngx-examples/calendar';
-import { CodeSnippetExamplesModule } from '@acpaas-ui/ngx-examples/code-snippet';
-import { FormsExamplesModule } from '@acpaas-ui/ngx-examples/forms';
 import { LocalstorageExamplesModule } from '@acpaas-ui/ngx-examples/localstorage';
 import { LogoExamplesModule } from '@acpaas-ui/ngx-examples/logo';
 
 export const ExamplesModules = [
 	AvatarExamplesModule,
 	CalendarExamplesModule,
-	CodeSnippetExamplesModule,
-	FormsExamplesModule,
 	LocalstorageExamplesModule,
 	LogoExamplesModule,
 ];
