828b88e21268f4b3639328f1441b97814d50a021
ICA\rc01673 <tom.vandenbroeck@digipolis.be>
2018-09-10 15:27:02 +0200

    more layout

--- packages/code-snippet/examples/code-snippet/pages/demo/demo.page.ts
+++ packages/code-snippet/examples/code-snippet/pages/demo/demo.page.ts
@@ -4,25 +4,24 @@ import { Component } from '@angular/core';
 	templateUrl: './demo.page.html',
 })
 export class CodeSnippetDemoPageComponent {
-	public codeExampleJSON = `
-	[
-	    {
-	        "title": "apples",
-	        "count": [12000, 20000],
-	        "description": {"text": "...", "sensitive": false}
-	    },
-	    {
-	        "title": "oranges",
-	        "count": [17500, null],
-	        "description": {"text": "...", "sensitive": false}
-	    }
-	]`;
+	public codeExampleJSON = `[
+	{
+		"title": "apples",
+		"count": [12000, 20000],
+		"description": {"text": "...", "sensitive": false}
+	},
+	{
+		"title": "oranges",
+		"count": [17500, null],
+		"description": {"text": "...", "sensitive": false}
+	}
+]`;
 
 	public codeExampleJS = `
-	function greetMe(yourName) {
-        alert('Hello ' + yourName);
-    }
-	greetMe('World');`;
+function greetMe(yourName) {
+	alert('Hello ' + yourName);
+}
+greetMe('World');`;
 
 	public importModule = 'import { CodeSnippetModule } from \'@acpaas-ui/ngx-components/code-snippet\';';
 	public example1 = '<aui-code-snippet [codeSnippet]="Single text example"></aui-code-snippet>';
