0cbc715b58e358e54592b9862660aa428b5332f9
ICA\rc01673 <tom.vandenbroeck@digipolis.be>
2018-09-10 10:30:56 +0200

    no auto-complete

--- packages/forms/examples/forms/forms.module.ts
+++ packages/forms/examples/forms/forms.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AutoCompleteModule } from '@acpaas-ui/ngx-components/forms';
 import { CodeSnippetModule } from '@acpaas-ui/ngx-components/code-snippet';
 
 import { Pages } from './pages/index';
-// console.log(AutoCompleteModule);
 @NgModule({
 	imports: [
 		CommonModule,
-		// AutoCompleteModule,
 		CodeSnippetModule,
 	],
 	declarations: [
