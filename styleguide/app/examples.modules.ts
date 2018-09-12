108b49b34b2ff3adbba6cc8c8c21d25df66ca426
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-09-12 18:35:18 +0200

    fix(styleguide): update autogenerated files

--- styleguide/app/examples.modules.ts
+++ styleguide/app/examples.modules.ts
@@ -1,11 +1,13 @@
-import { AvatarExamplesModule } from '@acpaas-ui/ngx-examples/avatar';
-import { CalendarExamplesModule } from '@acpaas-ui/ngx-examples/calendar';
-import { LogoExamplesModule } from '@acpaas-ui/ngx-examples/logo';
-import { SelectableListExamplesModule } from '@acpaas-ui/ngx-examples/selectable-list';
+import { AvatarExamplesModule } from '@acpaas-ui/ngx-examples/avatar/fesm2015/avatar';
+import { CalendarExamplesModule } from '@acpaas-ui/ngx-examples/calendar/fesm2015/calendar';
+import { LogoExamplesModule } from '@acpaas-ui/ngx-examples/logo/fesm2015/logo';
+import { SelectableListExamplesModule } from '@acpaas-ui/ngx-examples/selectable-list/fesm2015/selectable-list';
+import { TableExamplesModule } from '@acpaas-ui/ngx-examples/table/fesm2015/table';
 
 export const ExamplesModules = [
 	AvatarExamplesModule,
 	CalendarExamplesModule,
 	LogoExamplesModule,
 	SelectableListExamplesModule,
+	TableExamplesModule,
 ];
--- styleguide/app/examples.routes.ts
+++ styleguide/app/examples.routes.ts
@@ -1,11 +1,13 @@
-import { AVATAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/avatar';
-import { CALENDAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/calendar';
-import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo';
-import { SELECTABLE_LIST_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/selectable-list';
+import { AVATAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/avatar/fesm2015/avatar';
+import { CALENDAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/calendar/fesm2015/calendar';
+import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo/fesm2015/logo';
+import { SELECTABLE_LIST_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/selectable-list/fesm2015/selectable-list';
+import { TABLE_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/table/fesm2015/table';
 
 export const EXAMPLES_ROUTES = [
 	{ path: 'avatar', children: AVATAR_EXAMPLES_ROUTES, title: 'Avatar', },
 	{ path: 'calendar', children: CALENDAR_EXAMPLES_ROUTES, title: 'Calendar', },
 	{ path: 'logo', children: LOGO_EXAMPLES_ROUTES, title: 'Logo', },
 	{ path: 'selectable-list', children: SELECTABLE_LIST_EXAMPLES_ROUTES, title: 'Selectable list', },
+	{ path: 'table', children: TABLE_EXAMPLES_ROUTES, title: 'Table', },
 ];
