2b3da8fab5492d963de6babaa127a8b603a21090
Jasper Van Proeyen <trianglejuice@mac.com>
2018-09-14 15:23:47 +0200

    docs(avatar): Updated avatar documentation

--- packages/avatar/examples/avatar/pages/demo/demo.page.ts
+++ packages/avatar/examples/avatar/pages/demo/demo.page.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
 	templateUrl: './demo.page.html',
 })
 export class AvatarDemoPageComponent {
-	public importModule = 'import { AvatarModule } from \'@acpaas-ui/ngx-components/avatar\';';
+	public importModule = `import { AvatarModule } from '@acpaas-ui/ngx-components/avatar';
+
+@NgModule({
+	imports: [
+		AvatarModule
+	]
+});
+
+export class AppModule {};`;
+
 	public example1 = '<aui-avatar image="https://robohash.org/acpaas-ui" title="My image"></aui-avatar>';
+
 	public example2 = '<aui-avatar icon="fa fa-user" title="An icon" size="L"></aui-avatar>';
+
 	public example3 = '<aui-avatar letter="T" title="The letter T" size="S"></aui-avatar>';
 }
