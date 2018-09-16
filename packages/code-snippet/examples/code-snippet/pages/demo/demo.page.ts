3eb0441a1e85e927280d4d03067132e05c03bcce
ICA\rc01673 <tom.vandenbroeck@digipolis.be>
2018-09-16 11:33:18 +0200

    Good commit

--- packages/code-snippet/examples/code-snippet/pages/demo/demo.page.ts
+++ packages/code-snippet/examples/code-snippet/pages/demo/demo.page.ts
@@ -5,21 +5,21 @@ import { Component } from '@angular/core';
 })
 export class CodeSnippetDemoPageComponent {
 	public codeExampleJSON = `[
-	{
-		"title": "apples",
-		"count": [12000, 20000],
-		"description": {"text": "...", "sensitive": false}
-	},
-	{
-		"title": "oranges",
-		"count": [17500, null],
-		"description": {"text": "...", "sensitive": false}
-	}
+    {
+        "title": "apples",
+        "count": [12000, 20000],
+        "description": {"text": "...", "sensitive": false}
+    },
+    {
+        "title": "oranges",
+        "count": [17500, null],
+        "description": {"text": "...", "sensitive": false}
+    }
 ]`;
 
 	public codeExampleJS = `
 function greetMe(yourName) {
-	alert('Hello ' + yourName);
+    alert('Hello ' + yourName);
 }
 greetMe('World');`;
 
@@ -27,14 +27,69 @@ greetMe('World');`;
 import { CodeSnippetModule } from \'@acpaas-ui/ngx-components/code-snippet\';
 
 @NgModule({
-	imports: [
-		CodeSnippetModule
-	]
+    imports: [
+        CodeSnippetModule
+    ]
 })
 
 export class AppModule {}`;
 
-	public example1 = '<aui-code-snippet [codeSnippet]="Single text example"></aui-code-snippet>';
-	public example2 = '<aui-code-snippet [codeSnippet]="codeExampleJSON"></aui-code-snippet>';
-	public example3 = '<aui-code-snippet [codeSnippet]="codeExampleJS"></aui-code-snippet>';
+	public html1 = `<div class="u-margin-bottom">
+    <div class="u-margin-bottom-xs">
+        <h4><aui-code-snippet [codeSnippet]="'Single text example'"></aui-code-snippet></h4>
+    </div>
+</div>`;
+
+	public html2 = `public codeExampleJSON = [
+    {
+        "title": "apples",
+        "count": [12000, 20000],
+        "description": {"text": "...", "sensitive": false}
+    },
+    {
+        "title": "oranges",
+        "count": [17500, null],
+        "description": {"text": "...", "sensitive": false}
+    }
+];`;
+
+	public html3 = `public codeExampleJS =
+function greetMe(yourName) {
+    alert('Hello ' + yourName);
+}
+greetMe('World');`;
+
+	// public example1 = '<aui-code-snippet [codeSnippet]="Single text example"></aui-code-snippet>';
+	// public example2 = '<aui-code-snippet [codeSnippet]="codeExampleJSON"></aui-code-snippet>';
+	// public example3 = '<aui-code-snippet [codeSnippet]="codeExampleJS"></aui-code-snippet>';
+
+	public example1 = `<div class="u-margin-bottom">
+    <div class="u-margin-bottom-xs">
+        <aui-code-snippet [codeSnippet]="'codeExample JS'"></aui-code-snippet>
+    </div>
+    <aui-code-snippet [codeSnippet]="example3"></aui-code-snippet>
+    <div class="u-margin-bottom-xs">
+        <aui-code-snippet [codeSnippet]="codeExampleJS"></aui-code-snippet>
+    </div>
+</div>`;
+
+	public example2 = `<div class="u-margin-bottom">
+    <div class="u-margin-bottom-xs">
+        <aui-code-snippet [codeSnippet]="'codeExample JS'"></aui-code-snippet>
+    </div>
+    <aui-code-snippet [codeSnippet]="example3"></aui-code-snippet>
+    <div class="u-margin-bottom-xs">
+        <aui-code-snippet [codeSnippet]="codeExampleJS"></aui-code-snippet>
+    </div>
+</div>`;
+
+	public example3 = `<div class="u-margin-bottom">
+    <div class="u-margin-bottom-xs">
+        <aui-code-snippet [codeSnippet]="'codeExample JS'"></aui-code-snippet>
+    </div>
+    <aui-code-snippet [codeSnippet]="example3"></aui-code-snippet>
+    <div class="u-margin-bottom-xs">
+        <aui-code-snippet [codeSnippet]="codeExampleJS"></aui-code-snippet>
+    </div>
+</div>`;
 }
