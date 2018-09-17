e37f1f4eb8973a5846b2bb8ede3bf19c59f1f200
Lieven Van Gestel <lieven.vangestel@digipolis.be>
2018-09-17 12:46:34 +0200

    chore(map): update example

--- packages/map/examples/map/pages/demo/demo.page.ts
+++ packages/map/examples/map/pages/demo/demo.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { LeafletMap } from '@acpaas-ui/ngx-components/map';
 
 @Component({
 	templateUrl: './demo.page.html',
 })
 export class DemoPageComponent {
+
+	public leafletMap: LeafletMap = new LeafletMap({
+		zoom: 13, // default zoom level
+		center: [51.215, 4.425], // default center point
+		onAddPolygon: (layer) => {},
+		onAddLine: (layer) => {},
+		onEditFeature: (feature) => {},
+});
 
 }
