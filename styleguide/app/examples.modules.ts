5f17abfe044c49602de515e3f7fa6e6154712024
ICA\rc01673 <tom.vandenbroeck@digipolis.be>
2018-09-17 13:51:06 +0200

    WIP merge tested + readme

--- styleguide/app/examples.modules.ts
+++ styleguide/app/examples.modules.ts
@@ -1,13 +1,21 @@
 import { AvatarExamplesModule } from '@acpaas-ui/ngx-examples/avatar/fesm2015/avatar';
 import { CalendarExamplesModule } from '@acpaas-ui/ngx-examples/calendar/fesm2015/calendar';
+import { FlyoutExamplesModule } from '@acpaas-ui/ngx-examples/flyout/fesm2015/flyout';
+import { LocalstorageExamplesModule } from '@acpaas-ui/ngx-examples/localstorage/fesm2015/localstorage';
 import { LogoExamplesModule } from '@acpaas-ui/ngx-examples/logo/fesm2015/logo';
+import { PaginationExamplesModule } from '@acpaas-ui/ngx-examples/pagination/fesm2015/pagination';
+import { ProgressBarExamplesModule } from '@acpaas-ui/ngx-examples/progress-bar/fesm2015/progress-bar';
 import { SelectableListExamplesModule } from '@acpaas-ui/ngx-examples/selectable-list/fesm2015/selectable-list';
 import { TableExamplesModule } from '@acpaas-ui/ngx-examples/table/fesm2015/table';
 
 export const ExamplesModules = [
 	AvatarExamplesModule,
 	CalendarExamplesModule,
+	FlyoutExamplesModule,
+	LocalstorageExamplesModule,
 	LogoExamplesModule,
+	PaginationExamplesModule,
+	ProgressBarExamplesModule,
 	SelectableListExamplesModule,
 	TableExamplesModule,
 ];
--- styleguide/app/examples.routes.ts
+++ styleguide/app/examples.routes.ts
@@ -1,13 +1,21 @@
 import { AVATAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/avatar/fesm2015/avatar';
 import { CALENDAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/calendar/fesm2015/calendar';
+import { FLYOUT_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/flyout/fesm2015/flyout';
+import { LOCALSTORAGE_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/localstorage/fesm2015/localstorage';
 import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo/fesm2015/logo';
+import { PAGINATION_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/pagination/fesm2015/pagination';
+import { PROGRESS_BAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/progress-bar/fesm2015/progress-bar';
 import { SELECTABLE_LIST_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/selectable-list/fesm2015/selectable-list';
 import { TABLE_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/table/fesm2015/table';
 
 export const EXAMPLES_ROUTES = [
 	{ path: 'avatar', children: AVATAR_EXAMPLES_ROUTES, title: 'Avatar', },
 	{ path: 'calendar', children: CALENDAR_EXAMPLES_ROUTES, title: 'Calendar', },
+	{ path: 'flyout', children: FLYOUT_EXAMPLES_ROUTES, title: 'Flyout', },
+	{ path: 'localstorage', children: LOCALSTORAGE_EXAMPLES_ROUTES, title: 'Localstorage', },
 	{ path: 'logo', children: LOGO_EXAMPLES_ROUTES, title: 'Logo', },
+	{ path: 'pagination', children: PAGINATION_EXAMPLES_ROUTES, title: 'Pagination', },
+	{ path: 'progress-bar', children: PROGRESS_BAR_EXAMPLES_ROUTES, title: 'Progress bar', },
 	{ path: 'selectable-list', children: SELECTABLE_LIST_EXAMPLES_ROUTES, title: 'Selectable list', },
 	{ path: 'table', children: TABLE_EXAMPLES_ROUTES, title: 'Table', },
 ];
