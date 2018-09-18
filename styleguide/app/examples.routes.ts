2acdcf8ff44e4c5553ed1793e68afc1f8ea843c8
Lieven Van Gestel <lieven.vangestel@digipolis.be>
2018-09-18 23:30:06 +0200

    fix(forms): Build fixed for most forms components, but styleguide not working yet

--- styleguide/app/examples.routes.ts
+++ styleguide/app/examples.routes.ts
@@ -4,6 +4,7 @@ import { FLYOUT_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/flyout/fesm2015/
 import { FORMS_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/forms/fesm2015/forms';
 import { LAYOUT_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/layout/fesm2015/layout';
 import { LOGO_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/logo/fesm2015/logo';
+import { MAP_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/map/fesm2015/map';
 import { PAGINATION_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/pagination/fesm2015/pagination';
 import { PROGRESS_BAR_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/progress-bar/fesm2015/progress-bar';
 import { SELECTABLE_LIST_EXAMPLES_ROUTES } from '@acpaas-ui/ngx-examples/selectable-list/fesm2015/selectable-list';
@@ -16,6 +17,7 @@ export const EXAMPLES_ROUTES = [
 	{ path: 'forms', children: FORMS_EXAMPLES_ROUTES, title: 'Forms', },
 	{ path: 'layout', children: LAYOUT_EXAMPLES_ROUTES, title: 'Layout', },
 	{ path: 'logo', children: LOGO_EXAMPLES_ROUTES, title: 'Logo', },
+	{ path: 'map', children: MAP_EXAMPLES_ROUTES, title: 'Map', },
 	{ path: 'pagination', children: PAGINATION_EXAMPLES_ROUTES, title: 'Pagination', },
 	{ path: 'progress-bar', children: PROGRESS_BAR_EXAMPLES_ROUTES, title: 'Progress bar', },
 	{ path: 'selectable-list', children: SELECTABLE_LIST_EXAMPLES_ROUTES, title: 'Selectable list', },
