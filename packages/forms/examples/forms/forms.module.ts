ac607eda55aacd995b32e847af6d3e61131b0471
Tom Van den Broeck <tom.vandenbroeck@digipolis.be>
2018-09-27 14:07:13 +0200

    router gone

--- packages/forms/examples/forms/forms.module.ts
+++ packages/forms/examples/forms/forms.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CodeSnippetModule } from '@acpaas-ui/ngx-components/code-snippet';
 import { FormsModule } from '@angular/forms';
 import { RangeSliderModule } from '@acpaas-ui/ngx-components/forms';
-import { RouterModule } from '@angular/router';
 
 import { Pages } from './pages/index';
 
@@ -13,7 +12,6 @@ import { Pages } from './pages/index';
 		FormsModule,
 		RangeSliderModule,
 		CodeSnippetModule,
-		RouterModule,
 	],
 	declarations: [
 		Pages,
