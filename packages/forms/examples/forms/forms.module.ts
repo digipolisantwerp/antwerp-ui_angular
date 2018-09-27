1f7eaa71768d0a73e78808403a75ccc0309d71f6
Tom Van den Broeck <tom.vandenbroeck@digipolis.be>
2018-09-27 08:48:07 +0200

    readme + example

--- packages/forms/examples/forms/forms.module.ts
+++ packages/forms/examples/forms/forms.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeSnippetModule } from '@acpaas-ui/ngx-components/code-snippet';
-// import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { MaskModule } from '@acpaas-ui/ngx-components/forms';
 import { RouterModule } from '@angular/router';
 
@@ -11,7 +11,7 @@ import { Pages } from './pages/index';
 	imports: [
 		CommonModule,
 		CodeSnippetModule,
-		// FormsModule,
+		FormsModule,
  		RouterModule,
  		MaskModule,
 	],
--- packages/forms/src/lib/mask/README.md
+++ packages/forms/src/lib/mask/README.md
@@ -32,15 +32,9 @@ import { MaskModule } from '@acpaas-ui/ngx-components/forms';
 export class AppModule {};
 ```
 
-```typescript
-public exampleCode = `<div class="a-input">
-	<input type="text" placeholder="dd/mm/yyyy" auiMask="99/99/9999" />
-</div>`;
-```
-
 ```html
 <div class="a-input">
-    <input type="text" placeholder="dd/mm/yyyy" auiMask="99/99/9999" />
+	<input type="text" placeholder="dd/mm/yyyy" auiMask="99/99/9999" />
 </div>
 ```
 
