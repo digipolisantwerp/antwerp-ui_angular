a69b4f844d2b892a98a12c907bf7756ba2fe6a89
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-10-09 16:28:53 +0200

    fix(styleguide): remove fesm2015 from import paths

--- styleguide/app/aui.modules.ts
+++ styleguide/app/aui.modules.ts
@@ -1,6 +1,6 @@
-import { ContextModule } from '@acpaas-ui/ngx-components/context/fesm2015/context';
-import { HeaderModule, FooterModule } from '@acpaas-ui/ngx-components/layout/fesm2015/layout';
-import { LogoModule } from '@acpaas-ui/ngx-components/logo/fesm2015/logo';
+import { ContextModule } from '@acpaas-ui/ngx-components/context';
+import { HeaderModule, FooterModule } from '@acpaas-ui/ngx-components/layout';
+import { LogoModule } from '@acpaas-ui/ngx-components/logo';
 
 export const AUIModules = [
 	ContextModule.forRoot({
