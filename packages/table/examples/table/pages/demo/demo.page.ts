50a424734a4533a8c521fd5fbb69822c85b5c3df
Tom Van den Broeck <tom.vandenbroeck@digipolis.be>
2018-10-11 09:58:41 +0200

    Fix more lintr issues

--- packages/table/examples/table/pages/demo/demo.page.ts
+++ packages/table/examples/table/pages/demo/demo.page.ts
@@ -37,64 +37,64 @@ export class TableDemoPageComponent {
 
 	public rows = [
 		{
-			"id": 0,
-			"firstName": "Wyatt",
-			"lastName": "Cooper",
-			"registeredAt": "Sat Feb 07 1981 01:04:46 GMT+0000 (UTC)"
+			'id': 0,
+			'firstName': 'Wyatt',
+			'lastName': 'Cooper',
+			'registeredAt': 'Sat Feb 07 1981 01:04:46 GMT+0000 (UTC)',
 		},
 		{
-			"id": 1,
-			"firstName": "Mullen",
-			"lastName": "Ballard",
-			"registeredAt": "Fri Aug 31 2001 06:47:22 GMT+0000 (UTC)"
+			'id': 1,
+			'firstName': 'Mullen',
+			'lastName': 'Ballard',
+			'registeredAt': 'Fri Aug 31 2001 06:47:22 GMT+0000 (UTC)',
 		},
 		{
-			"id": 2,
-			"firstName": "Sonia",
-			"lastName": "Bass",
-			"registeredAt": "Sat Jul 12 1975 16:00:43 GMT+0000 (UTC)"
+			'id': 2,
+			'firstName': 'Sonia',
+			'lastName': 'Bass',
+			'registeredAt': 'Sat Jul 12 1975 16:00:43 GMT+0000 (UTC)',
 		},
 		{
-			"id": 3,
-			"firstName": "Kristen",
-			"lastName": "Moore",
-			"registeredAt": "Mon Nov 09 2015 16:11:21 GMT+0000 (UTC)"
+			'id': 3,
+			'firstName': 'Kristen',
+			'lastName': 'Moore',
+			'registeredAt': 'Mon Nov 09 2015 16:11:21 GMT+0000 (UTC)',
 		},
 		{
-			"id": 4,
-			"firstName": "Moss",
-			"lastName": "Bowen",
-			"registeredAt": "Thu Aug 04 1977 05:52:52 GMT+0000 (UTC)"
+			'id': 4,
+			'firstName': 'Moss',
+			'lastName': 'Bowen',
+			'registeredAt': 'Thu Aug 04 1977 05:52:52 GMT+0000 (UTC)',
 		},
 		{
-			"id": 5,
-			"firstName": "Elaine",
-			"lastName": "Michael",
-			"registeredAt": "Wed Mar 30 1977 01:48:30 GMT+0000 (UTC)"
+			'id': 5,
+			'firstName': 'Elaine',
+			'lastName': 'Michael',
+			'registeredAt': 'Wed Mar 30 1977 01:48:30 GMT+0000 (UTC)',
 		},
 		{
-			"id": 6,
-			"firstName": "Jerri",
-			"lastName": "Hicks",
-			"registeredAt": "Wed Jul 10 2013 22:53:48 GMT+0000 (UTC)"
+			'id': 6,
+			'firstName': 'Jerri',
+			'lastName': 'Hicks',
+			'registeredAt': 'Wed Jul 10 2013 22:53:48 GMT+0000 (UTC)',
 		},
 		{
-			"id": 7,
-			"firstName": "Sharron",
-			"lastName": "Castro",
-			"registeredAt": "Mon Sep 27 1976 07:55:10 GMT+0000 (UTC)"
+			'id': 7,
+			'firstName': 'Sharron',
+			'lastName': 'Castro',
+			'registeredAt': 'Mon Sep 27 1976 07:55:10 GMT+0000 (UTC)'
 		},
 		{
-			"id": 8,
-			"firstName": "Harriett",
-			"lastName": "Horton",
-			"registeredAt": "Wed Aug 18 2010 14:06:33 GMT+0000 (UTC)"
+			'id': 8,
+			'firstName': 'Harriett',
+			'lastName': 'Horton',
+			'registeredAt': 'Wed Aug 18 2010 14:06:33 GMT+0000 (UTC)',
 		},
 		{
-			"id": 9,
-			"firstName": "Griffin",
-			"lastName": "Navarro",
-			"registeredAt": "Tue Oct 24 2017 23:45:35 GMT+0000 (UTC)"
+			'id': 9,
+			'firstName': 'Griffin',
+			'lastName': 'Navarro',
+			'registeredAt': 'Tue Oct 24 2017 23:45:35 GMT+0000 (UTC)',
 		}
 	];
 
@@ -150,68 +150,68 @@ public columns: TableColumn[] = [
 
 public rows = [
 	{
-		"id": 0,
-		"firstName": "Wyatt",
-		"lastName": "Cooper",
-		"registeredAt": "Sat Feb 07 1981 01:04:46 GMT+0000 (UTC)"
+		'id': 0,
+		'firstName': 'Wyatt',
+		'lastName': 'Cooper',
+		'registeredAt': 'Sat Feb 07 1981 01:04:46 GMT+0000 (UTC)',
 	},
 	{
-		"id": 1,
-		"firstName": "Mullen",
-		"lastName": "Ballard",
-		"registeredAt": "Fri Aug 31 2001 06:47:22 GMT+0000 (UTC)"
+		'id': 1,
+		'firstName': 'Mullen',
+		'lastName': 'Ballard',
+		'registeredAt': 'Fri Aug 31 2001 06:47:22 GMT+0000 (UTC)',
 	},
 	{
-		"id": 2,
-		"firstName": "Sonia",
-		"lastName": "Bass",
-		"registeredAt": "Sat Jul 12 1975 16:00:43 GMT+0000 (UTC)"
+		'id': 2,
+		'firstName': 'Sonia',
+		'lastName': 'Bass',
+		'registeredAt': 'Sat Jul 12 1975 16:00:43 GMT+0000 (UTC)',
 	},
 	{
-		"id": 3,
-		"firstName": "Kristen",
-		"lastName": "Moore",
-		"registeredAt": "Mon Nov 09 2015 16:11:21 GMT+0000 (UTC)"
+		'id': 3,
+		'firstName': 'Kristen',
+		'lastName': 'Moore',
+		'registeredAt': 'Mon Nov 09 2015 16:11:21 GMT+0000 (UTC)',
 	},
 	{
-		"id": 4,
-		"firstName": "Moss",
-		"lastName": "Bowen",
-		"registeredAt": "Thu Aug 04 1977 05:52:52 GMT+0000 (UTC)"
+		'id': 4,
+		'firstName': 'Moss',
+		'lastName': 'Bowen',
+		'registeredAt': 'Thu Aug 04 1977 05:52:52 GMT+0000 (UTC)',
 	},
 	{
-		"id": 5,
-		"firstName": "Elaine",
-		"lastName": "Michael",
-		"registeredAt": "Wed Mar 30 1977 01:48:30 GMT+0000 (UTC)"
+		'id': 5,
+		'firstName': 'Elaine',
+		'lastName': 'Michael',
+		'registeredAt': 'Wed Mar 30 1977 01:48:30 GMT+0000 (UTC)',
 	},
 	{
-		"id": 6,
-		"firstName": "Jerri",
-		"lastName": "Hicks",
-		"registeredAt": "Wed Jul 10 2013 22:53:48 GMT+0000 (UTC)"
+		'id': 6,
+		'firstName': 'Jerri',
+		'lastName': 'Hicks',
+		'registeredAt': 'Wed Jul 10 2013 22:53:48 GMT+0000 (UTC)',
 	},
 	{
-		"id": 7,
-		"firstName": "Sharron",
-		"lastName": "Castro",
-		"registeredAt": "Mon Sep 27 1976 07:55:10 GMT+0000 (UTC)"
+		'id': 7,
+		'firstName': 'Sharron',
+		'lastName': 'Castro',
+		'registeredAt': 'Mon Sep 27 1976 07:55:10 GMT+0000 (UTC)'
 	},
 	{
-		"id": 8,
-		"firstName": "Harriett",
-		"lastName": "Horton",
-		"registeredAt": "Wed Aug 18 2010 14:06:33 GMT+0000 (UTC)"
+		'id': 8,
+		'firstName': 'Harriett',
+		'lastName': 'Horton',
+		'registeredAt': 'Wed Aug 18 2010 14:06:33 GMT+0000 (UTC)',
 	},
 	{
-		"id": 9,
-		"firstName": "Griffin",
-		"lastName": "Navarro",
-		"registeredAt": "Tue Oct 24 2017 23:45:35 GMT+0000 (UTC)"
+		'id': 9,
+		'firstName': 'Griffin',
+		'lastName': 'Navarro',
+		'registeredAt': 'Tue Oct 24 2017 23:45:35 GMT+0000 (UTC)',
 	}
 ];
 
-public loading: boolean = false;
+public loading = false;
 
 constructor(
 	private datePipe: DatePipe
@@ -228,7 +228,7 @@ constructor(
 </aui-table>
 	`;
 
-	public loading: boolean = false;
+	public loading = false;
 
 	constructor(
 		private datePipe: DatePipe
