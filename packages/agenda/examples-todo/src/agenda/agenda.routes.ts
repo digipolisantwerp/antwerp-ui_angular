bd6a0f7d729f83360d53c00ee703ea23e8da0242
Jasper De Smet <jasp.desmet@gmail.com>
2018-11-28 11:46:12 +0100

    refactor(agenda) undo comment route

--- packages/agenda/examples-todo/src/agenda/agenda.routes.ts
+++ packages/agenda/examples-todo/src/agenda/agenda.routes.ts
@@ -3,9 +3,9 @@ import { Routes } from '@angular/router';
 import { AgendaDemoPageComponent } from './pages/demo/demo.page';
 
 export const AGENDA_EXAMPLES_ROUTES: Routes = [
-	// {
-	// 	path: '',
-	// 	component: AgendaDemoPageComponent,
-	// 	pathMatch: 'full',
-	// },
+	{
+		path: '',
+		component: AgendaDemoPageComponent,
+		pathMatch: 'full',
+	},
 ];
