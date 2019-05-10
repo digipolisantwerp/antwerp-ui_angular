d0cfbd0f8ecf4855bde1ed9ae7420388a38603dc
Jasper Van Proeyen <trianglejuice@mac.com>
2019-05-10 09:20:06 +0200

    * Updated ACPaaS branding * Cleanup

--- packages/forms/lib/src/wysiwyg/wysiwyg.conf.ts
+++ packages/forms/lib/src/wysiwyg/wysiwyg.conf.ts
@@ -1,6 +1,6 @@
 export const WYSIWYG_DEFAULT_CONFIG = {
 	bodyClass: 'a-input ckeditor-editable-body',
-	contentsCss: ['https://cdn.antwerpen.be/core_branding_scss/3.0.1/main.min.css'],
+	contentsCss: ['https://cdn.antwerpen.be/core_branding_scss/3.2.1/main.min.css'],
 	format_tags: 'p;h1;h2;h3;h4;h5;h6',
 	toolbar_Basic: [
 		[ 'Bold', 'Italic', 'Underline', '-', 'Format', '-', 'Source' ],
