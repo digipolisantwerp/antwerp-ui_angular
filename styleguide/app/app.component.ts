f5262299972aae37aac464f25c7d5af0f96160d3
Jasper Van Proeyen <trianglejuice@mac.com>
2019-11-08 10:28:40 +0100

    * feat(core) Added accessibility features to the styleguide * fix(filter) Fixed semantic markup

--- styleguide/app/app.component.ts
+++ styleguide/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+	Component,
+	ElementRef,
+	Renderer,
+	ViewChild,
+} from '@angular/core';
 import { Routes } from '@angular/router';
 
 import { EXAMPLES_ROUTES } from './examples.routes';
@@ -9,5 +14,19 @@ import { EXAMPLES_ROUTES } from './examples.routes';
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+	constructor(
+		private renderer: Renderer
+	) {}
+
+	@ViewChild('mainContent') mainContent: ElementRef;
+	@ViewChild('skipContent') skipContent: ElementRef;
+
 	public packages: Routes = EXAMPLES_ROUTES;
+
+	public goToMainContent() {
+		const mainElement = this.mainContent.nativeElement;
+		const skipElement = this.skipContent.nativeElement;
+		this.renderer.invokeElementMethod(mainElement, 'focus');
+		skipElement.blur();
+	}
 }
