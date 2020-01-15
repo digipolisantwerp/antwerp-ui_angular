aa8e9049e45854ba5834e66d8865b8938c56805d
Michael <castiau.m@gmail.com>
2020-01-15 15:13:21 +0100

    Fix run unit testing

--- packages/navigation-menu/src/lib/navigation-menu/components/menu-tab/menu-tab.component.spec.ts
+++ packages/navigation-menu/src/lib/navigation-menu/components/menu-tab/menu-tab.component.spec.ts
@@ -1,7 +1,7 @@
 import { MenuService } from '../../services/menu.service';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import * as sinon from 'sinon';
-import COMPONENTS from '..';
+import { COMPONENTS } from '../index';
 import { hot, cold, getTestScheduler } from 'jasmine-marbles';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -10,218 +10,218 @@ import { Menu } from '../../interfaces';
 import { MenuTabComponent } from './menu-tab.component';
 
 describe('Menu Tab Component Test', () => {
-  let service: MenuService;
-  let fixture: ComponentFixture<MenuTabComponent>;
-  let component: MenuTabComponent;
+	let service: MenuService;
+	let fixture: ComponentFixture<MenuTabComponent>;
+	let component: MenuTabComponent;
 
-  let state$: Observable<Menu.MenuState>;
-  let closeAllMenus$: Observable<void>;
+	let state$: Observable<Menu.MenuState>;
+	let closeAllMenus$: Observable<void>;
 
-  let sandbox: sinon.SinonSandbox;
+	let sandbox: sinon.SinonSandbox;
 
-  beforeEach(async () => {
-    sandbox = sinon.createSandbox();
-    await TestBed.configureTestingModule({
-      declarations: [
-        ...COMPONENTS
-      ],
-      providers: [
-        {
-          provide: 'config',
-          useValue: {
-            dockedByDefault: false
-          }
-        },
-        {
-          provide: MenuService,
-          useValue: sinon.createStubInstance(MenuService)
-        }
-      ],
-      imports: [
-        RouterModule,
-        CommonModule
-      ]
-    }).compileComponents();
+	beforeEach(async () => {
+		sandbox = sinon.createSandbox();
+		await TestBed.configureTestingModule({
+			declarations: [
+				...COMPONENTS,
+			],
+			providers: [
+				{
+					provide: 'config',
+					useValue: {
+						dockedByDefault: false,
+					},
+				},
+				{
+					provide: MenuService,
+					useValue: sinon.createStubInstance(MenuService),
+				},
+			],
+			imports: [
+				RouterModule,
+				CommonModule,
+			],
+		}).compileComponents();
 
-    service = TestBed.get(MenuService);
+		service = TestBed.get(MenuService);
 
-    /*
+		/*
     DEFAULT CONFIG
     */
-    state$ = hot('-');
-    closeAllMenus$ = cold('-');
+		state$ = hot('-');
+		closeAllMenus$ = cold('-');
 
-    sinon.stub(service, 'state$').get(() => state$);
-    sinon.stub(service, 'displaySubMenu$').get(() => cold('-'));
-    sinon.stub(service, 'onCloseMenu$').get(() => closeAllMenus$);
-    sinon.stub(service, 'rootTemplateRef$').get(() => cold('-'));
-    sinon.stub(service, 'currentMenuIsNestedSubMenu$').get(() => cold('-'));
-    (service as any).configuration = { dockedByDefault: false };  // Overwrite prop
+		sinon.stub(service, 'state$').get(() => state$);
+		sinon.stub(service, 'displaySubMenu$').get(() => cold('-'));
+		sinon.stub(service, 'onCloseMenu$').get(() => closeAllMenus$);
+		sinon.stub(service, 'rootTemplateRef$').get(() => cold('-'));
+		sinon.stub(service, 'currentMenuIsNestedSubMenu$').get(() => cold('-'));
+		(service as any).configuration = { dockedByDefault: false };  // Overwrite prop
 
-    fixture = TestBed.createComponent(MenuTabComponent);
-    component = fixture.componentInstance;
-  });
+		fixture = TestBed.createComponent(MenuTabComponent);
+		component = fixture.componentInstance;
+	});
 
-  describe('Initialization', () => {
-    it('should create a valid component', () => {
-      expect(fixture).toBeDefined();
-      expect(component).toBeDefined();
-      fixture.detectChanges();
-    });
-  });
+	describe('Initialization', () => {
+		it('should create a valid component', () => {
+			expect(fixture).toBeDefined();
+			expect(component).toBeDefined();
+			fixture.detectChanges();
+		});
+	});
 
-  describe('Active State', () => {
-    it('should have default inactive state', () => {
-      state$ = hot('--a', {
-        a: {
-          docked: false,
-          mode: 'mobile',
-          activeMenu: 'false'
-        }
-      });
-      fixture.detectChanges();
-      expect(component.tabIsActive$).toBeObservable(cold('a', { a: false }));
-    });
-    it('should become active on click', () => {
-      component.headerClicked$ = hot('--a');
-      fixture.detectChanges();
-      expect(component.tabIsActive$).toBeObservable(cold('a-b', { a: false, b: true }));
-    });
+	describe('Active State', () => {
+		it('should have default inactive state', () => {
+			state$ = hot('--a', {
+				a: {
+					docked: false,
+					mode: 'mobile',
+					activeMenu: 'false',
+				},
+			});
+			fixture.detectChanges();
+			expect(component.tabIsActive$).toBeObservable(cold('a', { a: false }));
+		});
+		it('should become active on click', () => {
+			component.headerClicked$ = hot('--a');
+			fixture.detectChanges();
+			expect(component.tabIsActive$).toBeObservable(cold('a-b', { a: false, b: true }));
+		});
 
-    it('should toggle active on multiple clicks', () => {
-      component.headerClicked$ = hot('---a----b');
-      fixture.detectChanges();
-      expect(component.tabIsActive$).toBeObservable(cold('a--b----c', { a: false, b: true, c: false }));
-    });
+		it('should toggle active on multiple clicks', () => {
+			component.headerClicked$ = hot('---a----b');
+			fixture.detectChanges();
+			expect(component.tabIsActive$).toBeObservable(cold('a--b----c', { a: false, b: true, c: false }));
+		});
 
-    it('should become inactive on close all menus', () => {
-      component.headerClicked$ = hot('a');
-      closeAllMenus$ = hot('------a');
-      fixture.detectChanges();
-      expect(component.tabIsActive$).toBeObservable(cold('(ab)--c', { a: false, b: true, c: false }));
-    });
+		it('should become inactive on close all menus', () => {
+			component.headerClicked$ = hot('a');
+			closeAllMenus$ = hot('------a');
+			fixture.detectChanges();
+			expect(component.tabIsActive$).toBeObservable(cold('(ab)--c', { a: false, b: true, c: false }));
+		});
 
-    it('should become inactive when opening up another (main) tab', () => {
-      component.headerClicked$ = hot('a');
-      state$ = hot('-------a', {
-        a: {
-          mode: 'mobile',
-          docked: 'false',
-          activeMenu: {
-            type: 'main',
-            menuItem: 'some-menu-item'
-          }
-        }
-      });
-      fixture.detectChanges();
-      expect(component.tabIsActive$).toBeObservable(cold('(ab)---c', { a: false, b: true, c: false }));
-    });
-    it('should NOT become inactive when opening a submenu', () => {
-      component.headerClicked$ = hot('a');
-      state$ = hot('-------a', {
-        a: {
-          mode: 'mobile',
-          docked: 'false',
-          activeMenu: {
-            type: 'submenu',
-            menuItem: 'some-menu-item'
-          }
-        }
-      });
-      fixture.detectChanges();
-      expect(component.tabIsActive$).toBeObservable(cold('(ab)----', { a: false, b: true }));
-    });
-  });
+		it('should become inactive when opening up another (main) tab', () => {
+			component.headerClicked$ = hot('a');
+			state$ = hot('-------a', {
+				a: {
+					mode: 'mobile',
+					docked: 'false',
+					activeMenu: {
+						type: 'main',
+						menuItem: 'some-menu-item',
+					},
+				},
+			});
+			fixture.detectChanges();
+			expect(component.tabIsActive$).toBeObservable(cold('(ab)---c', { a: false, b: true, c: false }));
+		});
+		it('should NOT become inactive when opening a submenu', () => {
+			component.headerClicked$ = hot('a');
+			state$ = hot('-------a', {
+				a: {
+					mode: 'mobile',
+					docked: 'false',
+					activeMenu: {
+						type: 'submenu',
+						menuItem: 'some-menu-item',
+					},
+				},
+			});
+			fixture.detectChanges();
+			expect(component.tabIsActive$).toBeObservable(cold('(ab)----', { a: false, b: true }));
+		});
+	});
 
-  describe('Active Menu', () => {
-    it('should set the active menu to main type when clicked', () => getTestScheduler().run((helpers) => {
-      component.headerClicked$ = hot('a');
-      fixture.detectChanges();
-      helpers.flush();
-      expect((service.updateState as sinon.SinonStub).withArgs('activeMenu', {
-        type: 'main',
-        menuItem: component
-      }).calledOnce).toBe(true);
-    }));
-    it('should not become inactive when active menu is same as current tab', () => {
-      component.headerClicked$ = hot('a');
-      state$ = hot('-----a', {
-        activeMenu: {
-          menuItem: component,
-          type: 'main'
-        }
-      });
-      fixture.detectChanges();
-      expect(component.tabIsActive$).toBeObservable(cold('(ab)--', { a: false, b: true }));
-    });
-  });
+	describe('Active Menu', () => {
+		it('should set the active menu to main type when clicked', () => getTestScheduler().run((helpers) => {
+			component.headerClicked$ = hot('a');
+			fixture.detectChanges();
+			helpers.flush();
+			expect((service.updateState as sinon.SinonStub).withArgs('activeMenu', {
+				type: 'main',
+				menuItem: component,
+			}).calledOnce).toBe(true);
+		}));
+		it('should not become inactive when active menu is same as current tab', () => {
+			component.headerClicked$ = hot('a');
+			state$ = hot('-----a', {
+				activeMenu: {
+					menuItem: component,
+					type: 'main',
+				},
+			});
+			fixture.detectChanges();
+			expect(component.tabIsActive$).toBeObservable(cold('(ab)--', { a: false, b: true }));
+		});
+	});
 
-  describe('Mobile Menu', () => {
-    it('should open main mobile menu on click', () => getTestScheduler().run((helpers) => {
-      state$ = hot('a', {
-        a: {
-          mode: 'mobile'
-        }
-      });
-      component.headerClicked$ = hot('--a', { a: false });
-      component.subMenu = { templateRef: 'template' as any } as any;
-      fixture.detectChanges();
-      helpers.flush();
+	describe('Mobile Menu', () => {
+		it('should open main mobile menu on click', () => getTestScheduler().run((helpers) => {
+			state$ = hot('a', {
+				a: {
+					mode: 'mobile',
+				},
+			});
+			component.headerClicked$ = hot('--a', { a: false });
+			component.subMenu = { templateRef: 'template' as any } as any;
+			fixture.detectChanges();
+			helpers.flush();
 
-      expect((service.displaySubMenu as sinon.SinonStub).withArgs({ type: 'main', templateRef: 'template' }).calledOnce).toBe(true);
-    }));
+			expect((service.displaySubMenu as sinon.SinonStub).withArgs({ type: 'main', templateRef: 'template' }).calledOnce).toBe(true);
+		}));
 
-    it('should open a submenu on click', () => getTestScheduler().run((helpers) => {
-      state$ = hot('a', {
-        a: {
-          mode: 'mobile'
-        }
-      });
-      component.headerClicked$ = hot('--a', { a: true });
-      component.subMenu = { templateRef: 'template' as any } as any;
-      fixture.detectChanges();
-      helpers.flush();
+		it('should open a submenu on click', () => getTestScheduler().run((helpers) => {
+			state$ = hot('a', {
+				a: {
+					mode: 'mobile',
+				},
+			});
+			component.headerClicked$ = hot('--a', { a: true });
+			component.subMenu = { templateRef: 'template' as any } as any;
+			fixture.detectChanges();
+			helpers.flush();
 
-      expect((service.displaySubMenu as sinon.SinonStub).withArgs({ type: 'submenu', templateRef: 'template' }).calledOnce).toBe(true);
-    }));
+			expect((service.displaySubMenu as sinon.SinonStub).withArgs({ type: 'submenu', templateRef: 'template' }).calledOnce).toBe(true);
+		}));
 
-    it('should not open mobile menu in desktop mode', () => getTestScheduler().run((helpers) => {
-      state$ = hot('a', {
-        a: {
-          mode: 'desktop'
-        }
-      });
-      component.headerClicked$ = hot('--a', { a: false });
-      component.subMenu = { templateRef: 'template' as any } as any;
-      fixture.detectChanges();
-      helpers.flush();
+		it('should not open mobile menu in desktop mode', () => getTestScheduler().run((helpers) => {
+			state$ = hot('a', {
+				a: {
+					mode: 'desktop',
+				},
+			});
+			component.headerClicked$ = hot('--a', { a: false });
+			component.subMenu = { templateRef: 'template' as any } as any;
+			fixture.detectChanges();
+			helpers.flush();
 
-      expect((service.displaySubMenu as sinon.SinonStub).called).toBe(false);
-    }));
+			expect((service.displaySubMenu as sinon.SinonStub).called).toBe(false);
+		}));
 
-    it('should not open a menu if no submenu is present', () => getTestScheduler().run((helpers) => {
-      state$ = hot('a', {
-        a: {
-          mode: 'mobile'
-        }
-      });
-      component.headerClicked$ = hot('a');
-      component.subMenu = undefined;
-      fixture.detectChanges();
-      helpers.flush();
-      expect((service.displaySubMenu as sinon.SinonStub).called).toBe(false);
-    }));
-    it('should not open a menu if no templateRef is present', () => getTestScheduler().run((helpers) => {
-      state$ = hot('a', {
-        a: {
-          mode: 'mobile'
-        }
-      });
-      component.headerClicked$ = hot('a');
-      component.subMenu = { templateRef: undefined } as any;
-      fixture.detectChanges();
-      helpers.flush();
-      expect((service.displaySubMenu as sinon.SinonStub).called).toBe(false);
-    }));
-  });
+		it('should not open a menu if no submenu is present', () => getTestScheduler().run((helpers) => {
+			state$ = hot('a', {
+				a: {
+					mode: 'mobile',
+				},
+			});
+			component.headerClicked$ = hot('a');
+			component.subMenu = undefined;
+			fixture.detectChanges();
+			helpers.flush();
+			expect((service.displaySubMenu as sinon.SinonStub).called).toBe(false);
+		}));
+		it('should not open a menu if no templateRef is present', () => getTestScheduler().run((helpers) => {
+			state$ = hot('a', {
+				a: {
+					mode: 'mobile',
+				},
+			});
+			component.headerClicked$ = hot('a');
+			component.subMenu = { templateRef: undefined } as any;
+			fixture.detectChanges();
+			helpers.flush();
+			expect((service.displaySubMenu as sinon.SinonStub).called).toBe(false);
+		}));
+	});
 });
--- packages/navigation-menu/src/lib/navigation-menu/components/menu/menu.component.spec.ts
+++ packages/navigation-menu/src/lib/navigation-menu/components/menu/menu.component.spec.ts
@@ -2,7 +2,7 @@ import { MenuService } from '../../services/menu.service';
 import { TestBed, ComponentFixture, tick } from '@angular/core/testing';
 import * as sinon from 'sinon';
 import { MenuComponent } from './menu.component';
-import COMPONENTS from '..';
+import { COMPONENTS } from '../index';
 import { hot, cold, getTestScheduler } from 'jasmine-marbles';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -10,121 +10,121 @@ import { Observable } from 'rxjs';
 import { Menu } from '../../interfaces';
 
 describe('Menu Component Test', () => {
-  let service: MenuService;
-  let fixture: ComponentFixture<MenuComponent>;
-  let component: MenuComponent;
+	let service: MenuService;
+	let fixture: ComponentFixture<MenuComponent>;
+	let component: MenuComponent;
 
-  let state$: Observable<Menu.MenuState>;
+	let state$: Observable<Menu.MenuState>;
 
-  let sandbox: sinon.SinonSandbox;
+	let sandbox: sinon.SinonSandbox;
 
-  beforeEach(async () => {
-    sandbox = sinon.createSandbox();
-    await TestBed.configureTestingModule({
-      declarations: [
-        ...COMPONENTS
-      ],
-      providers: [
-        {
-          provide: 'config',
-          useValue: {
-            dockedByDefault: false
-          }
-        },
-        {
-          provide: MenuService,
-          useValue: sinon.createStubInstance(MenuService)
-        }
-      ],
-      imports: [
-        RouterModule,
-        CommonModule
-      ]
-    }).compileComponents();
+	beforeEach(async () => {
+		sandbox = sinon.createSandbox();
+		await TestBed.configureTestingModule({
+			declarations: [
+				...COMPONENTS
+			],
+			providers: [
+				{
+					provide: 'config',
+					useValue: {
+						dockedByDefault: false
+					}
+				},
+				{
+					provide: MenuService,
+					useValue: sinon.createStubInstance(MenuService)
+				}
+			],
+			imports: [
+				RouterModule,
+				CommonModule
+			]
+		}).compileComponents();
 
-    service = TestBed.get(MenuService);
+		service = TestBed.get(MenuService);
 
     /*
     DEFAULT CONFIG
     */
-    state$ = hot('-');
+		state$ = hot('-');
 
-    sinon.stub(service, 'state$').get(() => state$);
-    sinon.stub(service, 'displaySubMenu$').get(() => cold('-'));
-    sinon.stub(service, 'onCloseMenu$').get(() => cold('-'));
-    sinon.stub(service, 'rootTemplateRef$').get(() => cold('-'));
-    sinon.stub(service, 'currentMenuIsNestedSubMenu$').get(() => cold('-'));
-    (service as any).configuration = { dockedByDefault: false };  // Overwrite prop
+		sinon.stub(service, 'state$').get(() => state$);
+		sinon.stub(service, 'displaySubMenu$').get(() => cold('-'));
+		sinon.stub(service, 'onCloseMenu$').get(() => cold('-'));
+		sinon.stub(service, 'rootTemplateRef$').get(() => cold('-'));
+		sinon.stub(service, 'currentMenuIsNestedSubMenu$').get(() => cold('-'));
+		(service as any).configuration = { dockedByDefault: false };  // Overwrite prop
 
-    fixture = TestBed.createComponent(MenuComponent);
-    component = fixture.componentInstance;
-  });
+		fixture = TestBed.createComponent(MenuComponent);
+		component = fixture.componentInstance;
+	});
 
-  afterEach(() => {
-    sandbox.restore();
-  });
+	afterEach(() => {
+		sandbox.restore();
+	});
 
-  describe('Initializion', () => {
-    it('should create a valid component with begin state NOT docked', () => getTestScheduler().run(() => {
-      expect(component).toBeDefined();
-      expect(component.isDocked).toBe(false);
-      state$ = hot('--a', {
-        a: {
-          docked: false
-        }
-      });
-      fixture.detectChanges();
+	describe('Initializion', () => {
+		it('should create a valid component with begin state NOT docked', () => getTestScheduler().run(() => {
+			expect(component).toBeDefined();
+			expect(component.isDocked).toBe(false);
+			state$ = hot('--a', {
+				a: {
+					docked: false
+				}
+			});
+			fixture.detectChanges();
 
-      // Since menu shouldn't be docked, we should show hide label (but it is delayed a bit)
-      expect(component.showHideMenuLabel$).toBeObservable(cold(`150ms --a`, { a: true }));
-      // Since we are running fake sync code, the delay is in here too
-      expect(component.showRevealMenuLabel$).toBeObservable(cold('150ms --a', { a: false }));
-    }));
-  });
+			// Since menu shouldn't be docked, we should show hide label (but it is delayed a bit)
+			expect(component.showHideMenuLabel$).toBeObservable(cold(`150ms --a`, { a: true }));
+			// Since we are running fake sync code, the delay is in here too
+			expect(component.showRevealMenuLabel$).toBeObservable(cold('150ms --a', { a: false }));
+		}));
+	});
 
-  describe('Undock and Dock', () => {
-    it('should show the right labels when going from undocked to docked', () => getTestScheduler().run((helpers) => {
-      state$ = hot('---a---b', { a: { docked: false }, b: { docked: true } });
-      fixture.detectChanges();
-      getTestScheduler().createTime('-------|');
-      helpers.flush();
+	describe('Undock and Dock', () => {
+		it('should show the right labels when going from undocked to docked', () => getTestScheduler().run((helpers) => {
+			state$ = hot('---a---b', { a: { docked: false }, b: { docked: true } });
+			fixture.detectChanges();
+			getTestScheduler().createTime('-------|');
+			helpers.flush();
 
-      expect(component.showHideMenuLabel$).toBeObservable(cold('150ms ---a', { a: false }));
-      // Show reveal is not delayed, but the delay is in here too since we are async testing
-      expect(component.showRevealMenuLabel$).toBeObservable(cold('150ms ---a', { a: true }));
-    }));
-    it('should show the right labels when going from docked to undocked', () => getTestScheduler().run((helpers) => {
-      state$ = hot('---a---b', { a: { docked: true }, b: { docked: false } });
-      fixture.detectChanges();
-      helpers.flush();
+			expect(component.showHideMenuLabel$).toBeObservable(cold('150ms ---a', { a: false }));
+			// Show reveal is not delayed, but the delay is in here too since we are async testing
+			expect(component.showRevealMenuLabel$).toBeObservable(cold('150ms ---a', { a: true }));
+		}));
+		it('should show the right labels when going from docked to undocked', () => getTestScheduler().run((helpers) => {
+			state$ = hot('---a---b', { a: { docked: true }, b: { docked: false } });
+			fixture.detectChanges();
+			helpers.flush();
 
-      expect(component.showHideMenuLabel$).toBeObservable(cold('150ms -------a 149ms b', { a: false, b: true }));
-    }));
+			expect(component.showHideMenuLabel$).toBeObservable(cold('150ms -------a 149ms b', { a: false, b: true }));
+		}));
 
-    it('should toggle docking', () => getTestScheduler().run((helpers) => {
-      state$ = hot('--a', { a: { docked: true } });
-      const spyOnUpdate = (service.updateState as sinon.SinonStub)
-        .withArgs('docked', false)
-        // Fake update the state
-        .callsFake(() => state$ = hot('--a', { a: { docked: false } }));
-      fixture.detectChanges();
-      component.toggleDocking();
-      helpers.flush();
-      expect(spyOnUpdate.withArgs('docked', false).calledOnce).toBe(true);
+		it('should toggle docking', () => getTestScheduler().run((helpers) => {
+			state$ = hot('--a', { a: { docked: true } });
+			const spyOnUpdate = (service.updateState as sinon.SinonStub)
+				.withArgs('docked', false)
+				// Fake update the state
+				.callsFake(() => state$ = hot('--a', { a: { docked: false } }));
+			fixture.detectChanges();
+			component.toggleDocking();
+			helpers.flush();
+			expect(spyOnUpdate.withArgs('docked', false).calledOnce).toBe(true);
 
-      // Now toggle it again
-      component.toggleDocking();
-      helpers.flush();
-      expect(spyOnUpdate.withArgs('docked', true).calledOnce).toBe(true);
-    }));
-  });
+			// Now toggle it again
+			component.toggleDocking();
+			helpers.flush();
+			expect(spyOnUpdate.withArgs('docked', true).calledOnce).toBe(true);
+		}));
+	});
 
-  describe('Destruction', () => {
-    it('should destroy the service when destroying itself', () => {
-      fixture.detectChanges();
-      const spyOnDestroy = (service.destroy as sinon.SinonStub);
-      component.ngOnDestroy();
-      expect(spyOnDestroy.calledOnce).toBe(true);
-    });
-  });
+	describe('Destruction', () => {
+		it('should destroy the service when destroying itself', () => {
+			fixture.detectChanges();
+			const spyOnDestroy = (service.destroy as sinon.SinonStub);
+			component.ngOnDestroy();
+			expect(spyOnDestroy.calledOnce).toBe(true);
+		});
+	});
 });
