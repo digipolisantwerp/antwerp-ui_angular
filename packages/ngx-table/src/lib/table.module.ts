9de2cf3a15ac8a21e01d3de3524f29293d285f8c
Michael Castiau <castiau.m@gmail.com>
2020-03-05 16:03:06 +0100

    Fix all other packages

--- packages/ngx-table/src/lib/table.module.ts
+++ packages/ngx-table/src/lib/table.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-
-import {Components} from './components';
-import {Directives} from './directives';
-
 import {TableHelperService} from './services/table-helper.service';
+import {ColumnSelectorComponent} from './components/column-selector/column-selector.component';
+import {TableComponent} from './components/table/table.component';
+import {TableBarComponent} from './components/table-bar/table-bar.component';
+import {TableCellComponent} from './components/table-cell/table-cell.component';
+import {TableHeaderComponent} from './components/table-header/table-header.component';
+import {TableBarItemDirective} from './directives/table-bar-item/table-bar-item.directive';
+import {TableBarSearchDirective} from './directives/table-bar-search/table-bar-search.directive';
 
 @NgModule({
   imports: [
@@ -16,12 +19,22 @@ import {TableHelperService} from './services/table-helper.service';
     TableHelperService,
   ],
   declarations: [
-    Components,
-    Directives,
+    ColumnSelectorComponent,
+    TableComponent,
+    TableBarComponent,
+    TableCellComponent,
+    TableHeaderComponent,
+    TableBarItemDirective,
+    TableBarSearchDirective,
   ],
   exports: [
-    Components,
-    Directives,
+    ColumnSelectorComponent,
+    TableComponent,
+    TableBarComponent,
+    TableCellComponent,
+    TableHeaderComponent,
+    TableBarItemDirective,
+    TableBarSearchDirective,
   ],
 })
 export class TableModule {
