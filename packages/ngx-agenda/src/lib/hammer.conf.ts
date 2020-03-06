d8f2075acf6f5f8fa1fc9f7a672294db35a03a5c
Michael Castiau <castiau.m@gmail.com>
2020-03-06 15:13:48 +0100

    small bugfix

--- packages/ngx-agenda/src/lib/hammer.conf.ts
+++ packages/ngx-agenda/src/lib/hammer.conf.ts
@@ -1,6 +1,7 @@
 import * as Hammer from 'hammerjs';
 import {HammerGestureConfig} from '@angular/platform-browser';
-
+import {Injectable} from '@angular/core';
+@Injectable()
 export class HammerConfig extends HammerGestureConfig {
   overrides = {
     swipe: {direction: Hammer.DIRECTION_ALL},
