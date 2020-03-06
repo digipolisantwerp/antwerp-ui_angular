fce456411aeef4081f1ceb853de0a0a0ffd1a666
Michael Castiau <castiau.m@gmail.com>
2020-03-06 14:15:50 +0100

    Fix build script

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -1,30 +1,105 @@
 const fs = require('fs');
 const path = require('path');
 const cli = require('child_process');
+const rxjs = require('rxjs');
+const operators = require('rxjs/operators');
+const cliProgress = require('cli-progress');
+const chalk = require('chalk');
+const rimraf = require('rimraf');
+
+/**
+ * This script basically runs 'ng build' for every
+ * library that is known in this project.
+ * It doesn't do any custom magic and is just
+ * more convenient to run than to manually run ng build for every
+ * package.
+ */
 
 const EXCLUDED_DIRS = [
-  'styleguide',
-  'ngx-notifications'
+  'styleguide', // Don't build the styleguide
+  'ngx-notifications' // This package was deprecated, but dir still exists
 ];
 
+const BUILD_AS_FIRST = [
+  'ngx-utils' // Basically every lib is dependent on this one, so build first
+];
+
+const BUILD_AS_LAST = [
+  'ngx-forms',
+  'ngx-layout',
+  'ngx-navigation-menu' // Menu is dependent on layout, so build as last
+];
+
+function clearDistFolder() {
+  console.log(chalk.yellow('\nClearing /dist directory...'));
+  return new rxjs.Observable((obs) => rimraf(path.resolve(__dirname, '../dist'), {}, (err) => {
+    if (err)
+      obs.error(err);
+
+    console.log(chalk.green('Dist directory cleared!'));
+    obs.next();
+  }));
+}
+
 function buildAngularPackage(packageName) {
-  return new Promise((resolve, reject) => {
-    cli.exec(`ng build ${packageName}`, error => error ? reject(error) : resolve())
+  return new rxjs.Observable((observer) => {
+    console.log(chalk.yellow(`\nBuilding ${packageName}...`));
+    const command = cli.exec(`ng build ${packageName}`, error => {
+      if (error) {
+        return observer.error(error);
+      }
+      console.log(chalk.green(`${packageName} built!`));
+      observer.next();
+      observer.complete();
+    });
   });
 }
 
 function getAllPackages() {
   const dirs = fs.readdirSync(path.resolve(__dirname, '../packages/'));
-  for (const EX of EXCLUDED_DIRS) {
+  return dirs;
+}
+
+function getMainPackages() {
+  const dirs = getAllPackages();
+  for (const EX of [...EXCLUDED_DIRS, ...BUILD_AS_LAST, ...BUILD_AS_FIRST]) {
     dirs.splice(dirs.findIndex(dir => dir === EX), 1);
   }
 
   return dirs;
 }
 
-function run() {
-  const packages = getAllPackages();
-  return Promise.all(packages.map(packageName => buildAngularPackage(packageName)))
+function welcomeMessage() {
+  console.log(chalk.cyan(`
+  ----------------ACPAAS-UI---------------
+  Angular Components Build
+  ----------------------------------------
+  `));
 }
 
-run();
+let bar;
+
+function run() {
+  welcomeMessage();
+  let progress = 0;
+  bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  bar.start(getAllPackages().length, 0);
+  const packages = getMainPackages();
+  const observables = [
+    ...BUILD_AS_FIRST.map(packageName => buildAngularPackage(packageName)),
+    ...packages.map(packageName => buildAngularPackage(packageName)),
+    ...BUILD_AS_LAST.map(packageName => buildAngularPackage(packageName))
+  ];
+  return clearDistFolder().pipe(
+    operators.take(1),
+    operators.switchMap(() => rxjs.concat(...observables)),
+    operators.tap(() => bar.update(++progress))
+  );
+}
+
+run().subscribe({
+  complete: () => {
+    bar.update(getAllPackages().length);
+    bar.stop()
+  }
+});
