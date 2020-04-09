99dbfff0ff52999d5c1ffa4b9d0aba1aa7c1e87a
Michael Castiau <castiau.m@gmail.com>
2020-04-09 11:28:08 +0200

    Export localstorage config

--- packages/ngx-localstorage/src/public-api.ts
+++ packages/ngx-localstorage/src/public-api.ts
@@ -3,3 +3,4 @@ import {LocalstorageService} from './lib/services/localstorage.service';
 
 export {LocalstorageModule, LocalstorageService};
 export {ILocalStorageConfig, IStorage, LocalStorageType} from './lib/types/localstorage.types';
+export {LOCALSTORAGE_CONFIG} from './lib/providers/localstorage.provider';
