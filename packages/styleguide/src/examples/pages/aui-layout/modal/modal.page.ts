b69e8ac1b4d0039345226d44a30ae2887f6e6745
Jasper Van Proeyen <trianglejuice@mac.com>
2020-11-19 17:06:06 +0100

    fix(core) Build fix

--- packages/styleguide/src/examples/pages/aui-layout/modal/modal.page.ts
+++ packages/styleguide/src/examples/pages/aui-layout/modal/modal.page.ts
@@ -12,66 +12,66 @@ export class LayoutModalDemoPageComponent {
 import { ModalAbstract, ModalService } from '@acpaas-ui/ngx-layout';
 
 @Component({
-	selector: 'aui-modal',
-	template: \`<div class="m-modal" role="dialog" aria-modal="true" aria-labelledby="myModalTitle" aria-describedby="myModelDesc">
-		<div class="m-modal__content">
-			<div class="m-modal__header u-margin-bottom-xs">
-				<button type="button" class="m-modal__close a-button-transparent a-button--default has-icon" (click)="closeModal()">
-					<span class="fa fa-close"></span>
-				</button>
-				<h4 id="myModalTitle">{{ modalData.title }}</h4>
-			</div>
-			<div class="u-margin-bottom">
-				<p id="myModalDesc">{{ modalData.text }}</p>
-			</div>
-			<div class="m-modal__footer">
-				<button type="button" class="a-button" (click)="submitAndCloseModal()">Close Modal</button>
-				<button type="button" class="a-button-outline" (click)="closeModal()">Cancel</button>
-			</div>
-		</div>
-	</div>\`,
+  selector: 'aui-modal',
+  template: \`<div class="m-modal" role="dialog" aria-modal="true" aria-labelledby="myModalTitle" aria-describedby="myModelDesc">
+    <div class="m-modal__content">
+      <div class="m-modal__header u-margin-bottom-xs">
+        <button type="button" class="m-modal__close a-button-transparent a-button--default has-icon" (click)="closeModal()">
+          <span class="fa fa-close"></span>
+        </button>
+        <h4 id="myModalTitle">{{ modalData.title }}</h4>
+      </div>
+      <div class="u-margin-bottom">
+        <p id="myModalDesc">{{ modalData.text }}</p>
+      </div>
+      <div class="m-modal__footer">
+        <button type="button" class="a-button" (click)="submitAndCloseModal()">Close Modal</button>
+        <button type="button" class="a-button-outline" (click)="closeModal()">Cancel</button>
+      </div>
+    </div>
+  </div>\`,
 })
 export class AUIDemoModalComponent extends ModalAbstract {
-	public modalData: any;
+  public modalData: any;
 
-	constructor(
-		public modalService: ModalService
-	) {
-		super(modalService);
-	}
+  constructor(
+    public modalService: ModalService
+  ) {
+    super(modalService);
+  }
 
-	 public submitAndCloseModal() {
-	 	console.log('Submit!');
-	 	this.closeModal();
-	 }
+   public submitAndCloseModal() {
+     console.log('Submit!');
+     this.closeModal();
+   }
 }
 `;
 
   public modal2 = `constructor(
-	private modalService: ModalService
+  private modalService: ModalService
 ) {}
 
 public openModal() {
-	this.modalService.openModal(
-		AUIDemoModalComponent,
-		{
-			title: 'Modal demo',
-			text: 'Are you sure you want to see a demo of this modal?',
-		}, {
-			confirm: () => this.doSomething(),
-		}
-	);
+  this.modalService.openModal(
+    AUIDemoModalComponent,
+    {
+      title: 'Modal demo',
+      text: 'Are you sure you want to see a demo of this modal?',
+    }, {
+      confirm: () => this.doSomething(),
+    }
+  );
 }
 
 private doSomething() {
-	return new Promise((resolve, reject) => {
-		return resolve();
-	});
+  return new Promise((resolve, reject) => {
+    return resolve();
+  });
 }`;
 
   public modal3 = `<button type="button" class="a-button"
-	(click)="openModal()">
-	Open modal
+  (click)="openModal()">
+  Open modal
 </button>`;
 
 public modal4 = `import { ApproveModalComponent } from '@acpaas-ui/ngx-layout';`;
@@ -124,14 +124,14 @@ public modal6 = `<button type="button" class="a-button"
 
   public openApproveModal() {
     this.modalService.openModal(ApproveModalComponent, {
-			question: 'Are you sure you want to close this demo modal?',
-			description: 'This action can be undone.',
-			approve: 'Close demo modal',
+      question: 'Are you sure you want to close this demo modal?',
+      description: 'This action can be undone.',
+      approve: 'Close demo modal',
       reject: 'Cancel'
-		}, {
-			approve: () => this.doSomething(),
-			reject: () => this.doSomethingElse()
-		});
+    }, {
+      approve: () => this.doSomething(),
+      reject: () => this.doSomethingElse()
+    });
   }
 
   private doSomething() {
